ea3194147bfbe05cf57a307ccae8bb5d0ade30c8
Avinash <palakurthiavinash07@gmail.com>
2023-08-04 15:32:25 +0530

    project started

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
+
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>OK</h1>
+    </div>
+  );
+}
+
+export default App;
